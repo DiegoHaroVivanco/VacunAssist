54a8686fd5151fd90aad8a7610353a9e4f4abf1a
Diego Haro <diego.harovivanco@gmail.com>
2022-06-19 23:46:23 -0300

    agregando configs

---
+++ database/config.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+module.exports = {
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": 0
+  },
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_DATABASE,
+    "dialect": "mysql",
+    "operatorsAliases": 0
+  }
+}
