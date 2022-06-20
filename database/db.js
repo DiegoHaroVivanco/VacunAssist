1a3d69efe392df2364c5b6848ad976f2ec506218
Diego Haro <diego.harovivanco@gmail.com>
2022-06-19 23:34:51 -0300

    agregando variables de entorno

--- database/db.js
+++ database/db.js
@@ -1,12 +1,20 @@
 const mysql = require('mysql')
+require('dotenv').config()
 
+// const conexion = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'lautaro08',
+//     database: 'Vacunassist'
+// })
 
 const conexion = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'lautaro08',
-    database: 'Vacunassist'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
 })
+
 
 conexion.connect((error) =>{
     if(error){
